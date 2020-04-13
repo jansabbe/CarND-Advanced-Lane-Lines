2ab0505d2528fdeb1f14c11cd707f0e84c299ab7
Jan Sabbe <jan.sabbe@atrenko.com>
2020-04-13 18:46:33 +0200

    Refactoring

--- camera_calibration.py
+++ camera_calibration.py
@@ -3,7 +3,6 @@ import numpy as np
 import cv2
 import glob
 from os import makedirs
-from os.path import basename
 
 chessboard_size = (9,6)
 
@@ -17,6 +16,15 @@ def image_size(img):
     (height, width) = img.shape[:2]
     return (width, height)
 
+def images_in_directory(directory):
+    return [cv2.imread(filename) for filename in glob.glob(f'{directory}/*.jpg')]
+
+def write_images_to_directory(images, name):
+    makedirs(f'output_images/{name}', exist_ok=True)
+    for idx, image in enumerate(images):
+        write_name = f'output_images/{name}/{name}_{idx+1}.jpg'
+        cv2.imwrite(write_name, image)
+
 class Calibration:
     def __init__(self, chessboard_size, image):
         self.image = image
@@ -26,23 +34,19 @@ class Calibration:
         self.found_corners, self.image_points = cv2.findChessboardCorners(grayscale, chessboard_size)
         self.object_points = create_object_points(chessboard_size)
 
-    def draw_corners(self, output_path):
+    @property
+    def image_with_corner(self):
         if self.found_corners:
             output_image = np.copy(self.image)
             cv2.drawChessboardCorners(output_image, self.chessboard_size, self.image_points, self.found_corners)
-            cv2.imwrite(output_path, output_image)
+            return output_image
+        return None
         
 
 class Camera:
-
-    @classmethod
-    def from_directory(cls, directory):
-        images = [cv2.imread(filename) for filename in glob.glob(f'{directory}/*.jpg')]
-        return cls(images)
-
     def __init__(self, images) -> None:
         self.calibration_images = images
-        makedirs('output_images/corners_found', exist_ok=True)
+        self.images_with_corners = []
         objpoints = []
         imgpoints = []
         img_size = None
@@ -51,25 +55,16 @@ class Camera:
             if calibration.found_corners:
                 objpoints.append(calibration.object_points)
                 imgpoints.append(calibration.image_points)
+                self.images_with_corners.append(calibration.image_with_corner)
                 img_size = calibration.img_size
         _, self.matrix, self.distortion_coefficients, _, _ = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
-
-    def draw_corners(self):
-        for idx, image in enumerate(self.calibration_images):
-            calibration = Calibration(chessboard_size, image)
-            if calibration.found_corners:
-                calibration.draw_corners(f'output_images/corners_found/found_corners_{idx}.jpg')
 
     def undistort(self, image):
         return cv2.undistort(image, self.matrix, self.distortion_coefficients)
 
 
 if __name__ == "__main__":
-    camera = Camera.from_directory('camera_cal')
-    camera.draw_corners()
-    images = glob.glob('test_images/*.jpg')
-    makedirs('output_images/undistorted', exist_ok=True)
-    for filename in images:
-        image = cv2.imread(filename)
-        write_name = f'output_images/undistorted/{basename(filename)}'
-        cv2.imwrite(write_name, camera.undistort(image))
+    camera = Camera(images_in_directory('camera_cal'))
+    write_images_to_directory(camera.images_with_corners, 'corners_found')
+    undistorted_images = [camera.undistort(image) for image in images_in_directory('camera_cal')]
+    write_images_to_directory(undistorted_images, 'undistorted')
