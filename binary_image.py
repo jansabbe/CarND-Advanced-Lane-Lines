0a8fd719517f6ace20fbe5bfa20a3eede6152f55
Jan Sabbe <jan.sabbe@atrenko.com>
2020-04-19 11:20:26 +0200

    Sobel on red channel instead of gray

--- binary_image.py
+++ binary_image.py
@@ -26,22 +26,21 @@ class BinaryImage:
         return np.array_equal(self.binary_image, other.binary_image)
 
 def absolute_sobel(image, orient='x', sobel_kernel=3):
-    grayscale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     orientation = [1, 0] if orient == 'x' else [0, 1]
-    derivative = cv2.Sobel(grayscale, cv2.CV_64F, *orientation, ksize=sobel_kernel)
+    derivative = cv2.Sobel(image, cv2.CV_64F, *orientation, ksize=sobel_kernel)
     absolute_derivative = np.absolute(derivative)
     return np.uint8(255 * absolute_derivative/np.max(absolute_derivative))
 
 def filter(image):
     red_image = image[:,:,0]
     saturation_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)[:, :, 2]
-    sobel_image = absolute_sobel(image, 'x', 9)
+    sobel_image = absolute_sobel(red_image, 'x', 9)
     red = BinaryImage.in_threshold(red_image, 40, 255)
     saturation = BinaryImage.in_threshold(saturation_image, 130, 255)
     sobel = BinaryImage.in_threshold(sobel_image, 50, 255)
-    return (red & saturation) | sobel
+    return ((red & saturation) | sobel).binary_image
 
 if __name__ == "__main__":
-    binary_images = [filter(image).binary_image for image in images_in_directory('output_images/bird_view')] 
+    binary_images = [filter(image) for image in images_in_directory('output_images/bird_view')] 
     write_images_to_directory(binary_images, 'binary_images', cmap='gray')
 
